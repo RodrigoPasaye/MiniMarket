6c6a2b41a2161f84fc109c712e90ae81d918f6c6
RodrigoPasaye <cv6200@innovaccion.mx>
2022-11-13 20:56:36 -0600

    Se hace referencia al formulario que se creo

--- MiniMarket.Presentacion/Program.cs
+++ MiniMarket.Presentacion/Program.cs
@@ -13,7 +13,7 @@ namespace MiniMarket.Presentacion {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmCategorias());
         }
     }
 }
