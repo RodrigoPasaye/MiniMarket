22bc166c4977c870cd3ae2dd3ee1c716699be055
RodrigoPasaye <cv6200@innovaccion.mx>
2022-11-13 20:44:02 -0600

    Se agrega la clase Conexion
    ** Contiene toda la informacion para generar la cadena de conexion

---
+++ MiniMarket.Datos/Conexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MiniMarket.Datos {
+  public class Conexion {
+    private string BD { get; set; }
+    private string Servidor { get; set; }
+    private string Usuario { get; set; }
+    private string Password { get; set; }
+    private bool Seguridad { get; set; }
+    private static Conexion Con = null;
+
+    private Conexion() {
+      this.BD = "DB_MINIMARKET";
+      this.Servidor = "DESKTOP-Q43GDQM\\SQLEXPRESS";
+      this.Usuario = "sa";
+      this.Password = "Abcde1";
+      this.Seguridad = false;
+    }
+
+    public SqlConnection CrearConexion() {
+      SqlConnection cadena = new SqlConnection();
+      try {
+        cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.BD + ";";
+        if (Seguridad) {
+          cadena.ConnectionString += "Integrated Security = SSPI";
+        } else {
+          cadena.ConnectionString = cadena.ConnectionString + "User Id=" + this.Usuario + "; Password=" + this.Password;
+        }
+      } catch (Exception ex) {
+        cadena = null;
+        throw ex;
+      }
+      return cadena;
+    }
+
+    public static Conexion GetInstancia() {
+      if (Con == null) {
+        Con = new Conexion();
+      }
+      return Con;
+    }
+  }
+}
