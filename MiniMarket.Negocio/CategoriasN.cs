ce25eca3b8a5d7b0c2a35517e26a1e10556db0f3
RodrigoPasaye <cv6200@innovaccion.mx>
2022-11-13 20:49:52 -0600

    Se agrega en la capa de negocio la clase de Categorias
    ** Aqui es donde se va a llamar a la capa de datos

---
+++ MiniMarket.Negocio/CategoriasN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MiniMarket.Entidades;
+using MiniMarket.Datos;
+
+namespace MiniMarket.Negocio {
+  public class CategoriasN {
+    public static DataTable ListadoCategorias(string id) {
+      CategoriasDao categoriasDao = new CategoriasDao();
+      return categoriasDao.ListadoCategorias(id);
+    }
+
+    public static string GuardarCategoria(int opcion, Categorias categorias) {
+      CategoriasDao categoriasDao = new CategoriasDao();
+      return categoriasDao.GuardarCategoria(opcion, categorias);
+    }
+  }
+}
